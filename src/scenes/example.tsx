e90531e57ad76b4f3329b05c3912c9c3f82ef46d
Joseph Abell <jsphabll@gmail.com>
2024-10-30 12:08:04 +0000

    logo

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -2,27 +2,31 @@ import {makeScene2D, CubicBezier, Circle} from '@motion-canvas/2d';
 import {createRef, all, tween, map} from '@motion-canvas/core';
 
 export default makeScene2D(function* (view) {
-  const column1 = -400;
-  const column2 = -200;
+  const column1 = -50;
+  const column2 = column1 + 100;
+  const row1 = 45;
+  const row2 = row1 - 100;
 
-  const row1 = 200;
-  const row2 = 100;
-
-  const circle = 15;
+  const circle = 20;
 
   const bezier1 = createRef<CubicBezier>();
   const bezier2 = createRef<CubicBezier>();
+
+  const holeColour = '#ddd';
+  const stringColour = '#d9d';
+
+
   view.add(
     <>
-      <Circle x={column1} y={row1} width={circle} height={circle} fill="#dddddd" />
-      <Circle x={column2} y={row1} width={circle} height={circle} fill="#dddddd" />
-      <Circle x={column1} y={row2} width={circle} height={circle} fill="#dddddd" />
-      <Circle x={column2} y={row2} width={circle} height={circle} fill="#dddddd" />
+      <Circle x={column1} y={row1} width={circle} height={circle} fill={holeColour} />
+      <Circle x={column2} y={row1} width={circle} height={circle} fill={holeColour} />
+      <Circle x={column1} y={row2} width={circle} height={circle} fill={holeColour} />
+      <Circle x={column2} y={row2} width={circle} height={circle} fill={holeColour} />
 
       <CubicBezier
         ref={bezier1}
         lineWidth={6}
-        stroke={'lightseagreen'}
+        stroke={stringColour}
         p0={[column1, row1]}
         p1={[column1, row1 + 40]}
         p2={[column2, row1 + 40]}
@@ -33,7 +37,7 @@ export default makeScene2D(function* (view) {
       <CubicBezier
         ref={bezier2}
         lineWidth={6}
-        stroke={'lightseagreen'}
+        stroke={stringColour}
         p0={[column2, row2]}
         p1={[column2, row2 + 140]}
         p2={[column1, row2 + 140]}
@@ -43,14 +47,16 @@ export default makeScene2D(function* (view) {
     </>
   );
 
-  yield* bezier1().end(1, 0.5);
-  yield* tween(
-    0.2,
-    (value) => bezier1()
-      .p1([column1, map(row1 + 40, row1, value)])
-      .p2([column2, map(row1 + 40, row1, value)])
+  yield* all(
+    bezier1().end(1, 0.3),
+    tween(
+      0.4,
+      (value) => bezier1()
+        .p1([column1, map(row1 + 40, row1, value)])
+        .p2([column2, map(row1 + 40, row1, value)])
+    )
   );
-  yield* bezier2().end(1, 0.5);
+  yield* bezier2().end(1, 0.3);
   yield* all(
     tween(
       0.2,
